373e825a95dd218bb122b5d48929308251625c01
Arthur Abelyan <a.abelyan@itransition.com>
2018-05-22 15:31:10 +0300

    initial commit

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export HOST_UID=$(id -u)
+
+# add project repository
+
+docker-compose build
+docker-compose up -d
+
+cat certificates/.env > app/.env
+sh run.sh php-cli composer install --no-interaction
+
+sleep 30s
+
+docker-compose restart supervisor
+
+sh run.sh php-cli bin/console doctrine:migrations:migrate --no-interaction
+sh run.sh php-cli bin/console doctrine:fixtures:load --no-interaction
